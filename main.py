3c290d535fbe5e6ca7c1b672674650ccbaaf3461
Logan Pedersen <logan@loganpedersen.com>
2017-12-23 15:47:57 -0700

    Added notable events print

--- main.py
+++ main.py
@@ -9,12 +9,15 @@ if __name__ == '__main__':
 
 		
 		for stock in lines:
-			rawData = getdata.stockData(stock, 2000, 2018)
+			rawData = getdata.stockData(stock, 2000, 2017)
 			diffData = stockAnalysis.getDiff(rawData)
 			resultData = stockAnalysis.getMeans(diffData)
 			print stock + ":"
 			
 			for season in resultData:
 				print season
+				for val, sd in season:
+					if abs(val) > sd :
+						print str(val) + ' ' + str(sd)
 			
 			print '\n'
